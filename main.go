fd17342d18f76f69d9fa38a9ac73f50b257eb42a
Joe Soares <error698@gmail.com>
2019-03-02 21:26:00 -0600

    initialize bot session
    -start bot session
    -registered ready handler
    -connect to web socket

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"discord-notify-bot/handlers"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func main() {
+	SetEnvVars()
+	// Create a new Discord session using the provided bot token.
+	dg, err := discordgo.New("Bot " + os.Getenv("BotAPIToken"))
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+
+	// Register handlers here
+	dg.AddHandler(handlers.Ready)
+
+	// Open a websocket connection to Discord and begin listening.
+	err = dg.Open()
+	if err != nil {
+		fmt.Println("error opening connection,", err)
+		return
+	}
+
+	// Wait here until CTRL-C or other term signal is received.
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
+	<-sc
+
+	// Cleanly close down the Discord session.
+	dg.Close()
+}
